11928006d41f727182fbbe489ad828a5d01844dd
Demo <demo@appsmith.com>
2023-07-13 12:17:00 +0000

    System generated commit, initial commit

---
+++ pages/dPage0/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+		showModal('Modal1');
+	},
+	myFun2: async () => {
+		//use async-await or promises
+		showModal(Modal1);
+		showModal('modal1')
+	}
+}
